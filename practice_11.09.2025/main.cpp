9fc8802dd79e8743841b2785715ae2a14f1f853a
Maxim_Simonov <simmaksimus@gmail.com>
2025-09-12 01:32:59 +0300

    CMD commit

--- practice_11.09.2025/main.cpp
+++ practice_11.09.2025/main.cpp
@@ -2,7 +2,6 @@
 #include <stdint.h>
 using namespace std;
 
-
 // краткое для копирования:
 // поставить 1 на позицию i в битовом числе A: A |= (1 << i) 
 // (поставит на позицию i + 1, то есть если i = 2, то поставит 1 в третий бит => число будет 4)
@@ -16,16 +15,22 @@ class My_Own_Set
 	// тогда нам нет смысла добавлять uint32 числа, потому что слишком 
 	// большие все равно не запихнутся в сет
 	uint16_t size;
+	uint16_t segments_count;
 	uint32_t* segments;
 public:
-	My_Own_Set(uint16_t s) : size(s) { segments = new uint32_t[size >> 5]; }
+	My_Own_Set(uint16_t s) : size(s) 
+	{ 
+		segments_count = (size + 31) >> 5;
+		segments = new uint32_t[segments_count](); 
+	}
 	~My_Own_Set() { delete[] segments; }
 
 	My_Own_Set(const My_Own_Set& copied_one)
 	{
 		size = copied_one.size;
-		segments = new uint32_t[size >> 5];
-		for (size_t i = 0; i < size; i++)
+		segments_count = copied_one.segments_count;
+		segments = new uint32_t[segments_count]();
+		for (size_t i = 0; i < segments_count; i++)
 			segments[i] = copied_one.segments[i];
 	}
 	My_Own_Set& operator=(const My_Own_Set& copied_one)
@@ -33,49 +38,90 @@ public:
 		if (this == &copied_one) return *this;
 		delete[] segments;
 		size = copied_one.size;
-		segments = new uint32_t[size >> 5];
-		for (size_t i = 0; i < size; i++)
+		segments_count = copied_one.segments_count;
+		segments = new uint32_t[segments_count]();
+		for (size_t i = 0; i < segments_count; i++)
 			segments[i] = copied_one.segments[i];
 	}
 
 	uint16_t get_size() const { return size; }
 	void set_size(uint16_t ns)
 	{
-		uint32_t* new_segments = new uint32_t[ns >> 5];
-		for (size_t i = 0; i < size; i++)
+		uint16_t nsc = (ns + 31) >> 5;
+		uint32_t* new_segments = new uint32_t[nsc]();
+		uint16_t min = (segments_count < nsc) ? segments_count : nsc;
+		for (size_t i = 0; i < min; i++)
 			new_segments[i] = segments[i];
 		delete[] segments;
 		size = ns;
+		segments_count = nsc;
 		segments = new_segments;
 	}
 
-	void add(uint16_t i) { segments[i >> 5] |= (1 << (i & ((1 << 5) - 1))); }
-	void del(uint16_t i) { segments[i >> 5] &= ~(1 << (i & ((1 << 5) - 1))); }
-	bool is_there(uint16_t i) { return segments[i >> 5] & (1 << i); }
-
-	My_Own_Set& association(const My_Own_Set& what_associate_with)
+	void add(uint16_t i) { if (i < size) segments[i >> 5] |= (1 << (i & 31)); }
+	void del(uint16_t i) { if (i < size) segments[i >> 5] &= ~(1 << (i & 31)); }
+	bool is_there(uint16_t i) const
 	{
-		for (size_t i = 0; i < size; i++)
+		if (i >= size) return false;
+		return segments[i >> 5] & (1 << (i & 31));
+	}
+
+	My_Own_Set& associateWith(const My_Own_Set& what_associate_with)
+	{
+		uint16_t min;
+		if (segments_count < what_associate_with.segments_count)
+			min = segments_count;
+		else min = what_associate_with.segments_count;
+		for (size_t i = 0; i < min; i++)
 			segments[i] |= what_associate_with.segments[i];
 		return *this;
 	}
-	My_Own_Set& intersection(const My_Own_Set& what_intersect_with)
+	My_Own_Set& intersectWith(const My_Own_Set& what_intersect_with)
 	{
-		for (size_t i = 0; i < size; i++)
+		uint16_t min;
+		if (segments_count < what_intersect_with.segments_count)
+			min = segments_count;
+		else min = what_intersect_with.segments_count;
+		for (size_t i = 0; i < min; i++)
 			segments[i] &= what_intersect_with.segments[i];
 		return *this;
 	}
-	My_Own_Set& negation()
+	My_Own_Set& negate()
 	{
-		for (size_t i = 0; i < size; i++)
+		for (size_t i = 0; i < segments_count; i++)
 			segments[i] = ~(segments[i]);
 		return *this;
 	}
 
+	static My_Own_Set association(const My_Own_Set& first, const My_Own_Set& second)
+	{
+		if (first.size >= second.size)
+			return My_Own_Set(first).associateWith(second);
+		return My_Own_Set(second).associateWith(first);
+	}
+	static My_Own_Set intersection(const My_Own_Set& first, const My_Own_Set& second)
+	{
+		if (first.size >= second.size)
+			return My_Own_Set(first).intersectWith(second);
+		return My_Own_Set(second).intersectWith(first);
+	}
+	static My_Own_Set negation(const My_Own_Set& what_to_negate)
+	{
+		return My_Own_Set(what_to_negate).negate();
+	}
+
 	void print_all()
 	{
-		for (size_t i = 0; i < size; i++)
-			if (this->is_there(i)) { cout << i; }
+		uint16_t s = size - 1; cout << "{";
+		bool flag = false;
+		for (size_t i = 0; i < s; i++)
+			if (this->is_there(i)) 
+			{ 
+				if (flag) cout << ", ";
+				cout << i; flag = true;
+			}
+		if (this->is_there(s)) { cout << ", " << s; }
+		cout << "}" << endl;
 	}
 };
 
@@ -101,10 +147,19 @@ int main()
 	cout << "Enter how many elems you want to add: "; cin >> ca; cout << endl;
 	cout << "Enter elems to add: " << endl;
 	for (size_t i = 0; i < ca; i++) { cin >> a; mos.add(a); }
+	mos.print_all();
 
 	cout << "Enter how many elems you want to delete: "; cin >> cd; cout << endl;
 	cout << "Enter elems to delete: " << endl;
 	for (size_t i = 0; i < cd; i++) { cin >> d; mos.del(d); }
+	mos.print_all();
+	cout << endl;
+	My_Own_Set mos2 = My_Own_Set::negation(mos);
+	mos2.print_all();
+	cout << endl;
+	My_Own_Set mos3 = My_Own_Set::association(mos, mos2).intersectWith(mos);
+	mos3.print_all();
+	cout << endl;
 	
 	return 0;
 }
